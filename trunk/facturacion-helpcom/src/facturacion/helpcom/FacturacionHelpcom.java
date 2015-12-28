92bf95cb1d0219b9d688982775943e15295a289a
Your Name <you@example.com>
2015-12-28 20:12:55 +0600

    init

---
+++ trunk/facturacion-helpcom/src/facturacion/helpcom/FacturacionHelpcom.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturacion.helpcom;
+
+/**
+ *
+ * @author helpcom
+ */
+public class FacturacionHelpcom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ trunk/facturacion-helpcom/src/facturacion/helpcom/PruebaCommit.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturacion.helpcom;
+
+/**
+ *
+ * @author helpcom
+ */
+public class PruebaCommit {
+    public void utaElLoco(){
+        
+    }
+}
